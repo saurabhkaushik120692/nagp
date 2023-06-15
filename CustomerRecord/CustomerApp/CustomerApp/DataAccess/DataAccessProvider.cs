3c054826d1841963840b6d787363dec9daf087f6
saurabhkaushik120692 <saurabh.kaushik1206@gmail.com>
2023-06-15 13:38:50 +0530

    Add files via upload
    Initial commit with running changes

---
+++ CustomerRecord/CustomerApp/CustomerApp/DataAccess/DataAccessProvider.cs
@@ -0,0 +1,26 @@
+﻿using CustomerApp.Model;
+
+namespace CustomerApp.DataAccess
+{
+    public class DataAccessProvider : IDataAccessProvider
+    {
+        private readonly PostgreSqlContext _context;
+
+        public DataAccessProvider(PostgreSqlContext context)
+        {
+            _context = context;
+        }
+
+        public void AddCustomerRecord(Customer customer)
+        {
+            _context.customer.Add(customer);
+            _context.SaveChanges();
+        }
+
+        public List<Customer> GetCustomerRecords()
+        {
+            var record = _context.customer.ToList();
+            return record;
+        }
+    }
+}
---
+++ CustomerRecord/CustomerApp/CustomerApp/DataAccess/PostgreSqlContext.cs
@@ -0,0 +1,26 @@
+﻿using CustomerApp.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerApp.DataAccess
+{
+    public class PostgreSqlContext : DbContext
+    {
+        public PostgreSqlContext(DbContextOptions<PostgreSqlContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> customer { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public override int SaveChanges()
+        {
+            ChangeTracker.DetectChanges();
+            return base.SaveChanges();
+        }
+    }
+}
